42195a19a0a5e10db6316783706c789c40455b70
Daniel Nguyen <daniel.n.q.nguyen@gmail.com>
2024-07-04 15:52:37 +1000

    Define spec

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Hack Night
+
+A little Rails project for a hack night exercise.
+
+## Spec
+
+- We can have a bunch of `Things`, where each has an `a` and a `b` Integer value.
+- There is a [table](truth_table.csv) that has a range of scores for each range of `a` and `b` values, e.g:
+  - if a Thing's `a=900`, `b=60`, its score would be **`0.861`**,
+    `score: 0.861, min_a: 800, max_a: 999, min_b: 50, max_b: 99`
+    `a` falls between this `min_a` and `max_a`, and `b` falls between `min_b` and `max_b`
+  - if a Thing's `a=900`, `b=49`, its score would be **`0.833`**
+    `score: 0.833, min_a: 800, max_a: 999, min_b: 1, max_b: 49`
+- for the response, Things must be sorted in descending order of score
+
+## Instructions
+
+- Don't edit `spec/requests/things_spec.rb`
+- Make the test pass
+- Feel free to move `truth_table.csv`
--- spec/requests/things_spec.rb
+++ spec/requests/things_spec.rb
@@ -4,16 +4,27 @@ require "rails_helper"
 
 RSpec.describe "Things API" do
   describe "GET /things" do
-    it "returns a 200 status" do
-      get "/things"
+    before do
+      create(:thing, name: "Thing 1", a: 2100, b: 40)
+      create(:thing, name: "Thing 2", a: 400, b: 90)
+      create(:thing, name: "Thing 3", a: 2800, b: 100)
+      create(:thing, name: "Thing 4", a: 11_000, b: 75)
+      create(:thing, name: "Thing 5", a: 5100, b: 50)
+    end
 
-      expect(response).to have_http_status(:success)
+    def expectation
+      [
+        { "name" => "Thing 2", "score" => 0.917 },
+        { "name" => "Thing 3", "score" => 0.750 },
+        { "name" => "Thing 1", "score" => 0.750 },
+        { "name" => "Thing 5", "score" => 0.611 }
+      ]
     end
 
     it "returns a list of things" do
-      get "/things"
+      get "/things", params: { size: 4 }
 
-      expect(response.parsed_body).to eq(["Things"])
+      expect(response.parsed_body).to eq(expectation)
     end
   end
 end
